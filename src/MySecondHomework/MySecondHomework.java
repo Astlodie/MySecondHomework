62cface9f1f7f26185c102fbd2ec10c03b1e2ad5
Astlodie <71849560+Astlodie@users.noreply.github.com>
2021-01-22 11:35:18 -0500

    My second homework (learn variables).

---
+++ src/MySecondHomework/MySecondHomework.java
@@ -0,0 +1,14 @@
+package MySecondHomework;
+public class MySecondHomework {
+static int m=100;
+	public static void main(String[] args) {
+		char [] str= {'a', 'b', 'c'};
+		System.out.println(str);
+		
+		String name= "Welcome!";
+		System.out.println(name);
+		
+		System.out.println(m);
+	}
+
+}
